b99ab2627e1440f91c4cb7c5244a6c9414ea12ce
Alan Santoyo <alansantoyo@Alans-MBP.dhost.uta.edu>
2024-01-19 10:54:57 -0600

    bonus checkpoint

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 class Hello {
 	public static void main(String[] arg) {
 		Scanner in = new Scanner(System.in);
+		String name;
 		System.out.print("What is your name? ");
+		name = in.nextLine();
 	}
 }
