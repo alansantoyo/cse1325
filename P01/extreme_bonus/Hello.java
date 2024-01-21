749ec84bedc3ff9a79d586881e4ecee6f66cef71
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-01-20 21:54:47 -0600

    extreme bonus checkpoint

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,5 +1,6 @@
-class Hello {
+public class Hello {
 	public static void main(String[] arg) {
 		String userInfo = System.getProperty("user.name");
+		System.out.println("Hello, " + userInfo);
 	}
 }
