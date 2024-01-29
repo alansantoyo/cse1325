4d42288d38decb45ae1a5c02b6575f2ff4e8ca53
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-01-29 13:28:08 -0600

    checkpoint

--- P02/full_credit/Note.java
+++ P02/full_credit/Note.java
@@ -2,7 +2,14 @@ public class Note {
 	private Pitch pitch;
 	private int octave;	
 
-	public Note {
+	public Note() {
 	
+	}
+	public Note(Pitch pitch, int octave) {
+
+	}
+
+	public String toString() {
+
 	}
 }
