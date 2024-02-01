b4cb1d0dae26ece41b5be1547b2389ca3c9e6204
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-01-31 20:56:28 -0600

    constructor shells done

--- P03/Product.java
+++ P03/Product.java
@@ -4,5 +4,18 @@ public class Product {
     private String name;
     private int price;
 
-    
+    public Product(String name, int price) {
+
+    }
+    public int getstockNumber() {
+
+    }
+    public int getPrice() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }    
 }
