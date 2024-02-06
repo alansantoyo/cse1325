41f7d057259db2b167771e9122d92ea5e9b42f46
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-05 18:32:18 -0600

    checkpoint

--- P03/Customer.java
+++ P03/Customer.java
@@ -3,7 +3,10 @@ public class Customer {
     private String email;
 
     public Customer(string name, string email) {
-
+        int i = email.indexOf('@');
+        if(i==-1 || (email.indexOf('@',i))==-1) {
+            throw new IllegalArgumentException("Invalid email address: " + email);
+        }
     }
 
     @Override
