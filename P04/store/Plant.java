a55ca3a753e21a76bb8d391361947a7418c7f8e8
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 20:38:26 -0600

    getExposure still to be done

--- P04/store/Plant.java
+++ P04/store/Plant.java
@@ -4,7 +4,8 @@ public class Plant extends Product {
     private Exposure exposure;
 
     public Plant(String species, int price, Exposure exposure) {
-
+        super("Plant: " + species,price);
+        this.exposure = exposure;
     }
     public Exposure getExposure() {
     
