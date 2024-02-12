b099d0f054c1dc4c7f6a9f3389d658d143f49557
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 21:22:07 -0600

    checkpoint

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,13 +1,16 @@
 package store;
+import java.util.ArrayList;
 
 public class Order {
     private static int nextOrderkNumber = 0;
     private int orderNumber;
-    private Item[] items;
+    private ArrayList<Item> items;
     private Customer customer;
     
     public Order(Customer customer) {
+        this.customer = customer;
         this.orderNumber = nextOrderkNumber++;
+        this.items = new ArrayList<>();
     }
     public addItem(Item item) {
     
