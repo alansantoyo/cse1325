40c6219fac48fb4b4e1aacb68ded222a1420a328
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 20:34:26 -0600

    field and constructor shell done

--- P04/store/Plant.java
+++ P04/store/Plant.java
@@ -1,5 +1,12 @@
 package store;
 
 public class Plant extends Product {
+    private Exposure exposure;
 
+    public Plant(String species, int price, Exposure exposure) {
+
+    }
+    public Exposure getExposure() {
+    
+    }
 }
