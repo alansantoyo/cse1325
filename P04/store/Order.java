1d333ee1b682a7ac39bc7a289c65a9543233dae9
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 20:53:20 -0600

    checkpoint

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -5,4 +5,14 @@ public class Order {
     private int orderNumber;
     private Item[] items;
     private Customer customer;
+    
+    public Order(Customer customer) {
+    }
+    public addItem(Item item) {
+    }
+    public int getPrice() {
+    }
+    @Override
+    public String toString() {
+    }
 }
