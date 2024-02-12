ea00fac1e78714ddac7e0ebda7a5c2cb132cadd5
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 21:08:50 -0600

    checkpoint

--- P04/store/Order.java
+++ P04/store/Order.java
@@ -1,18 +1,22 @@
 package store;
 
 public class Order {
-    private int nextOderkNumber;
+    private static int nextOrderkNumber = 0;
     private int orderNumber;
     private Item[] items;
     private Customer customer;
     
     public Order(Customer customer) {
+        this.orderNumber = nextOrderkNumber++;
     }
     public addItem(Item item) {
+    
     }
     public int getPrice() {
+    
     }
     @Override
     public String toString() {
+    
     }
 }
