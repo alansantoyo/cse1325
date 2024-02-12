f2491fa96f7bd1cf6011b506b4535a3055665fc4
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-11 20:24:13 -0600

    checkpoint

--- P04/store/Exposure.java
+++ P04/store/Exposure.java
@@ -1,5 +1,20 @@
 package store;
 
 public enum Exposure {
+    SHADE, 
+    PARTSUN, 
+    SUN;
 
+    @Override
+    public String toString() {
+        if(this == PARTSUN) {
+            return "part sun / part shade";
+        }
+        else if(this == SUN) {
+            return "full sun";
+        }
+        else if(this == SHADE) {
+            return "full shade";
+        }
+    }
 }
