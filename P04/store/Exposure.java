c8ee055dbf0b3d51e53a08de4e9bc53d713ecceb
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-12 21:36:37 -0600

    checkpoint

--- P04/store/Exposure.java
+++ P04/store/Exposure.java
@@ -1,10 +1,31 @@
 package store;
-
+/**
+ * Models an exposure enum
+ *
+ * @author            Alan Santoyo-Pina
+ * @version           0.2
+ * @since             0.2
+ * @license.agreement Gnu General Public License 3.0
+ */
 public enum Exposure {
-    SHADE, 
-    PARTSUN, 
+   /**
+     * Shade plant
+     */
+    SHADE,
+   /**
+     * PartSun plant
+     */ 
+    PARTSUN,
+   /** 
+     * Sun plant
+     */ 
     SUN;
-
+   /**
+     * Gives back the chose enum as a string
+     *
+     * @returns the desired string corresponding to the current exposure
+     * @since   0.2
+     */
     @Override
     public String toString() {
         if(this == PARTSUN) {
