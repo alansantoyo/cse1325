bf667b1bb1aa62ba5840d5c5510342a66154f6cb
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-12 21:19:09 -0600

    checkpoint

--- P04/store/Customer.java
+++ P04/store/Customer.java
@@ -1,9 +1,23 @@
 package store;
 
+/**
+ * Models a customer by name and email address.
+ *
+ * @author            Alan Santoyo-Pina
+ * @version           0.2
+ * @since             0.2
+ * @license.agreement Gnu General Public License 3.0
+ */
 public class Customer {
     private String name;
     private String email;
-
+   /**
+    * Creates a Customer instance
+    * 
+    * @param name  the customer's name
+    * @param email the customer's email
+    * @since       0.2 
+    */
     public Customer(String name, String email) {
         int i = email.indexOf('@');
         if(i==-1 || (email.indexOf('@',i))==-1) {
@@ -14,7 +28,12 @@ public class Customer {
             this.email = email;
         }
     }
-
+   /**
+    * Returns the formatted name and email
+    *
+    * @returns the name and email as one string
+    * since    0.2
+    */
     @Override
     public String toString() {
         return "" + this.name + " (" + this.email + ")";
