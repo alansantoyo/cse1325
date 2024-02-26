d337dbd8087f52c7a220c1b10a466f127b94d5a4
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-26 16:30:50 -0600

    checkpoint

--- P05/store/Store.java
+++ P05/store/Store.java
@@ -1,5 +1,10 @@
 package store;
+import java.util.ArrayList;
 
 public class Store {
+    private String name;
+    private ArrayList<Customer> customers;
+    private ArrayList<Product> products;
+    private ArrayList<Order> orders;
 
 }
