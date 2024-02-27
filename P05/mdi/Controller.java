ab70d0e160ac4a9ff337633d165364804acf9190
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-26 22:23:13 -0600

    checkpoint

--- P05/mdi/Controller.java
+++ P05/mdi/Controller.java
@@ -3,9 +3,57 @@ package mdi;
 public class Controller {
     private Store store;
     private View view;
-    private Menu, mainMenu;
+    private Menu mainMenu;
     private boolean isRunning;
     private Scanner in;
 
+    public Controller(String storeName) {
+        this.store = store;
+        this.view = view;
+        this.mainMenu = mainMenu;
+        this.isRunning = isRunning;
+        this.in = in;
+    }
+    public void mdi() {
+
+    }
+
+    private void exit() {
+
+    }
+    private void placeOrder() {
+
+    }
+    private void newCustomer() {
+
+    }
+    private void newTool() {
+
+    }
+    private void newPlant() {
+
+    }
+    private void switchView() {
+
+    }
+
+    private String getView() {
+
+    }
+    private int selectFromMenu() {
+
+    }
+    private void print(String s) {
+
+    }
+    private void getString(String prompt) {
+
+    }
+    private int getInt(String prompt) {
+
+    }
+    private double getDouble(String prompt) {
+
+    }
 
 }
