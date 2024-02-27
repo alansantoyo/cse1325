3baab80b1bc58a0d53eaaf9001baeb64eb734698
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-02-26 22:12:05 -0600

    checkpoint

---
+++ P05/mdi/Controller.java
@@ -0,0 +1,11 @@
+package mdi;
+
+public class Controller {
+    private Store store;
+    private View view;
+    private Menu, mainMenu;
+    private boolean isRunning;
+    private Scanner in;
+
+
+}
