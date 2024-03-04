ad2536aa64385c393075ac0a9871a33b5fddcec5
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-03-04 14:41:49 -0600

    checkpoint revert menu files

--- P05/mdi/Menu.java
+++ P05/mdi/Menu.java
@@ -1,23 +1,16 @@
-package mdi;
-
 /*
 Menu Manager Copyright (c) 2024 Professsor George F. Rice
-
 Licensed under The MIT License (MIT)
-
 Summary: Basically, you can do whatever you want as long as you include 
 this copyright and license notice in any copy of the software/source.
-
 Permission is hereby granted, free of charge, to any person obtaining a copy 
 of this software and associated documentation files (the "Software"), to deal 
 in the Software without restriction, including without limitation the rights 
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
 of the Software, and to permit persons to whom the Software is furnished to do so, 
 subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included 
 in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
 INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
 PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
@@ -26,6 +19,7 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+package mdi;
 import java.util.ArrayList;
 
 // This class manages the entire menu and dispatch table
@@ -48,5 +42,3 @@ class Menu {
         items.get(itemNumber).run();
     }
 }
-
-
