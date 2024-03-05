9e114f7aa71919b27d7244bc151dbcf727f616ff
Alan Santoyo <alansantoyo@Alans-MacBook-Pro.local>
2024-03-04 22:21:12 -0600

    checkpoint

--- P06/mdi/View.java
+++ P06/mdi/View.java
@@ -1,5 +1,10 @@
 package mdi;
 
+// Copyright 2024 by Professor George F. Rice, modifications copyright 2024 by Alan Santoyo-Pina
+// This program is free software: you can redistribute it and/or modify it under the terms 
+// of the GNU General Public License as published by the Free Software Foundation, either 
+// version 3 of the License, or (at your option) any later version.
+
 import store.Store;
 
 public enum View {
