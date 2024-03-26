3b32dfe9f9a97c21a5775c6a1b3bfbedb654ae91
Alan Santoyo <alansantoyo@Alans-MBP.dhost.uta.edu>
2024-03-26 16:26:02 -0500

    checkpoint

--- P08/full_credit/hello_all.cpp
+++ P08/full_credit/hello_all.cpp
@@ -3,8 +3,11 @@
 int main() {
     std::string your_name;
     std::cout << "What's your name? ";
-    std::cin >> your_name;
-    std::cout << "Hello, " << your_name << std::endl;
+
+    //std::cin >> your_name;
+    std::getline(std::cin,your_name);
+
+    std::cout << "Hello, " << your_name << "!" << std::endl;
 
     return 0;
 }
