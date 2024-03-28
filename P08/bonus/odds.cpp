f072528eac67312cc0cbf06bba39b1c2facdb77e
Alan Santoyo <alansantoyo@Alans-MBP.dhost.uta.edu>
2024-03-28 15:14:54 -0500

    checkpoint

--- P08/bonus/odds.cpp
+++ P08/bonus/odds.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-int main() {
+int main(int args, char* argv[]) {
+    std::vector<std::string> odds;
+    std::vector<std::string>* evens = new std::vector<std::string>; 
 
+    for(int i = 0; i < args; i++) {
+        std::string text = argv[i];
+        
+        std::cout << text << std::endl;
+    }
 
 
     return 0;
