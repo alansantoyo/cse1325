9edb0396ce7fb74e7b41e8c164548fa1779e6599
Alan Santoyo <alansantoyo@Alans-MBP.dhost.uta.edu>
2024-04-18 16:28:26 -0500

    checkpoint

---
+++ P11/full_credit/university.h
@@ -0,0 +1,16 @@
+#ifndef __UNIVERSITY_H
+#define __UNIVERSITY_H
+
+#include <string>
+
+class University {
+private:
+    int enrollment;
+    std::string enrollment;
+
+public:
+    
+
+};
+
+#endif
