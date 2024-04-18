84cfc083265c16dfe3a105bf393d74e7b30cc2e0
Alan Santoyo <alansantoyo@Alans-MBP.dhost.uta.edu>
2024-04-18 16:15:58 -0500

    checkpoint

--- P09/full_credit/linear.cpp
+++ P09/full_credit/linear.cpp
@@ -1,7 +1,7 @@
 #include "linear.h"
 
 Linear::Linear(std::vector<double>& coefficients)
-    : Polynomial(coefficients)       {
+    : Polynomial::Polynomial(coefficients)       {
     if((coefficients.size() != 2) || (coefficients[0] == 0)) {
         throw std::runtime_error("a may not be 0");
     }
@@ -10,6 +10,8 @@ Linear::~Linear(){}
 
 std::vector<double> Linear::solve() {
 
-    return ((-1)*coefficients[1])/coefficients[0];
+    double a = coefficients[0];
+    double b = coefficients[1];
 
+    return {-b/a};
 }
